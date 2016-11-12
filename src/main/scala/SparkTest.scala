56b4206a9f3c4bf56e2307420affa7f6a174bf2a
Chinna <Chinna>
2016-11-12 12:42:54 +0530

    Initial commit

---
+++ src/main/scala/SparkTest.scala
@@ -0,0 +1,16 @@
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by Dell on 07-Nov-2016.
+  */
+object SparkTest extends App{
+    println("Hello Spark 2.0")
+
+    val spark = SparkSession.builder().master(args(0))getOrCreate()
+    val ds = spark.read.option("header",true).csv(args(1))
+    ds.show()
+    spark.stop()
+    import spark.implicits._
+    val ds1 = (1 to 10).toDF("id")
+}
+
